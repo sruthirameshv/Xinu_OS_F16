05d652b3b8d690d04c02eac432bcc52ad233ff0d
rraavi <raghuveer.raavi@gmail.com>
2016-09-14 23:52:20 -0400

    Fixing more more more issues

--- shell/xsh_hello.c
+++ shell/xsh_hello.c
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-/*xsh_hello.c - xshhello*/
-#include <xinu.h>
-#include <stdio.h>
-#include <string.h>
-/*------------------------------------------------------------------------
- * xsh_cat - shell command to cat one or more files
- *------------------------------------------------------------------------
- */
-shellcmd xsh_hello(int nargs, char *args[]){
-	if (nargs == 2){
-           printf("Hello %s, Welcome to the world of Xinu!!", args[1]);
-		return 1;
-	}
-	else if( nargs > 2 ) {
-      		printf("Too many arguments supplied.\n");
-   		
-	}
-	else {
-      		printf("One argument expected.\n");
-
-	
-	return 0;
-}}
-=======
 /* xsh_hello.c - xsh_hello */
 
 #include <xinu.h>
@@ -43,6 +18,3 @@ shellcmd xsh_hello(int nargs, char *args[]){
 	}
 	return 0;
 }
-
->>>>>>> 8fd62a37dfa17b34d0f27731f06b5650ee27ec28
-
